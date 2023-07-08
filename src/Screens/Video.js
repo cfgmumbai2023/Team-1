ee25dd44c5e9f79abcaa963dda549132bc137853
triquetra271201 <77985724+triquetra271201@users.noreply.github.com>
2023-07-09 00:32:53 +0530

    video player raw

---
+++ src/Screens/Video.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "../Components/Search"
+import VideoPlayer from "../Components/VideoPlayer";
+
+export function Video(){
+    return(
+        <div>
+            <Search />
+            < VideoPlayer />
+        </div>
+    );
+}
